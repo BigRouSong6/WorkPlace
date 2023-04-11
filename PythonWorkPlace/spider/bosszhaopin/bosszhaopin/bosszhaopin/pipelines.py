aaceb16db10e607581a963d5c98a9318413fbd2d
BigRouSong6 <1225786502@qq.com>
2023-04-12 07:26:16 +0800

    Boss爬虫以及入库（scrapy、selenium、mysql）

--- PythonWorkPlace/spider/bosszhaopin/bosszhaopin/bosszhaopin/pipelines.py
+++ PythonWorkPlace/spider/bosszhaopin/bosszhaopin/bosszhaopin/pipelines.py
@@ -17,24 +17,36 @@ class BosszhaopinPipeline:
 
     def process_item(self, item, spider):
         try:
-            sql ='insert into jobdataspider \
-                (recruit_status,release_date,job_titel,company_name,company_number,industry,salary_range_high,salary_range_num,office_location,experience_limit,education_limit,benefits,key_words,description) \
-                    values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")',\
-                        [str(item['recruit_status']),str(item['release_date']),str(item['job_titel']),str(item['company_name']),str(item['company_number']),str(item['industry']),str(item['salary_range_high']),str(item['salary_range_num']),str(item['office_location']),str(item['experience_limit']),str(item['education_limit']),str(item['benefits']),str(item['key_words']),str(item['description'])]
-            self.cursor.execute(sql)
-            print('插入数据成功')
+            sql = 'insert into jobdataspider (job_titel,company_name,company_number,industry,salary_range_low,salary_range_high,salary_range_num,office_location,experience_limit,education_limit,benefits,key_words) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            # recruit_status = item['recruit_status']
+            # release_date = item['release_date']
+            job_titel = item['job_titel']
+            company_name = item['company_name']
+            company_number = item['company_number']
+            industry = item['industry']
+            salary_range_low = item['salary_range_low']
+            salary_range_high = item['salary_range_high']
+            salary_range_num = item['salary_range_num']
+            office_location = item['office_location']
+            experience_limit = item['experience_limit']
+            education_limit = item['education_limit']
+            benefits = item['benefits']
+            key_words = item['key_words']
+            # description = item['description']
+            data = (job_titel,company_name,company_number,industry,salary_range_low,salary_range_high,salary_range_num,office_location,experience_limit,education_limit,benefits,key_words)
+            self.cursor.execute(sql,data)
+            print('《---------------------------------插入数据成功')
+            print(data)
         except Exception as e:
             print('插入错误',e)
             self.mysql_con.rollback()
         else:
             self.mysql_con.commit()
-            print('提交成功')
-        return item
-    
+            print('---------------------------------提交成功》\n\n')
+            return item   #必须返回
 
 
-    def close(self,spider):
-        print('断开连接')
+    def close_spider(self,spider):
         self.cursor.close()
         self.mysql_con.close()
-        pass
+        print('断开数据库连接')
--- PythonWorkPlace/spider/bosszhaopin/bosszhaopin/bosszhaopin/spiders/boss_zhaopin1.py
+++ PythonWorkPlace/spider/bosszhaopin/bosszhaopin/bosszhaopin/spiders/boss_zhaopin1.py
@@ -3,8 +3,8 @@ from urllib import parse
 from selenium import webdriver
 from bosszhaopin.items import BosszhaopinItem
 
-
-key_word = "爬虫"
+print('请输入关键词（岗位名词/行业等）：')
+key_word = str(input())
 url_quote = 'https://www.zhipin.com/web/geek/job?city=101280600&query='+parse.quote(key_word)
 
 class BossZhaopin1Spider(scrapy.Spider):
@@ -19,6 +19,7 @@ class BossZhaopin1Spider(scrapy.Spider):
         self.options.add_argument('--ignore-certificate-errors')
         self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
         self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # self.options.add_argument('--headless')
         self.bro = webdriver.Chrome(executable_path='D:\其他杂物\chromedriver.exe',options = self.options)
 
     def parse(self, response):
@@ -26,22 +27,22 @@ class BossZhaopin1Spider(scrapy.Spider):
         try:
             li_list = response.xpath('//li[@class="job-card-wrapper"]')
             for li in li_list:
-                item['recruit_status'] = "None"
-                item['release_date'] = "None"
-                item['description'] = "None"
+                # item['recruit_status'] = "None"
+                # item['release_date'] = "None"
+                # item['description'] = "None"
                 item['job_titel']  = li.xpath('.//span[@class="job-name"]//text()').extract_first()
                 item['company_name']  = li.xpath('.//h3[@class="company-name"]//text()').extract_first()
                 item['company_number']  = li.xpath('.//ul[@class="company-tag-list"]/li[last()]/text()').extract_first()
                 item['industry']  = li.xpath('.//ul[@class="company-tag-list"]/li[1]/text()').extract_first()
-                item['salary_range_low']  = li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[0]
+                item['salary_range_low']  = int(li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[0])
                 try:
-                    item['salary_range_high']  = li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[1].split('K')[0]
+                    item['salary_range_high']  = int(li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[1].split('K')[0])
                 except:
-                    item['salary_range_high']  = li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[1].split('K')[0]
+                    item['salary_range_high']  = int(li.xpath('.//span[@class="salary"]//text()').extract_first().split("-")[1].split('K')[0])
                 try:
-                    item['salary_range_num']  = li.xpath('.//span[@class="salary"]//text()').extract_first().split("·")[1].split("薪")[0]
+                    item['salary_range_num']  = int(li.xpath('.//span[@class="salary"]//text()').extract_first().split("·")[1].split("薪")[0])
                 except:
-                    item['salary_range_num']  = '12'
+                    item['salary_range_num']  = 12
                 item['office_location']  = li.xpath('.//span[@class="job-area"]//text()').extract_first()
                 item['experience_limit']  = li.xpath('.//ul[@class="tag-list"]/li[1]/text()').extract_first()
                 item['education_limit']  = li.xpath('.//ul[@class="tag-list"]/li[last()]/text()').extract_first()
@@ -63,16 +64,14 @@ class BossZhaopin1Spider(scrapy.Spider):
             #翻页
         for page in range(2,11):
             url_next = url_quote+'&page='+parse.quote(str(page))
-            print('现在的页数：'+str(page))
             yield scrapy.Request(
                 url = url_next,
                 callback = self.parse
             )
-            print('此页处理完毕'+str(page))
 
         
 
-    def closed(self,spider):
-        self.bro.quit
+    def close(self,spider):
+        self.bro.quit()
         print('爬虫结束，关闭浏览器')
         time.sleep(1)
